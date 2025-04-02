e44ba4ca20dde15b585be5758a0769e225d140f3
Aswin-murali-444 <aswinmurali444@gmail.com>
2025-04-02 09:00:37 +0530

    Edit

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173', // Adjust as per frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
